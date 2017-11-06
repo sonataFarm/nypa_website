2da22bfcb41a64cf2ca0c7f73c69e529285873aa
Nate Festinger <nate.festinger@gmail.com>
2017-11-05 20:39:11 -0700

    Render into root div

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@
 var path = require('path');
 
 module.exports = {
-  entry: "./frontend/my_app.jsx",
+  entry: "./frontend/entry.jsx",
   output: {
       path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
       filename: "bundle.js"
