bb329d2b5899c0171830b5c17018399b6975a8ee
Nate Festinger <nate.festinger@gmail.com>
2017-11-12 17:03:11 -0800

    Change users to admins

---
+++ app/models/admin.rb
@@ -0,0 +1,3 @@
+class Admin < ApplicationRecord
+  # TODO
+end
--- test/controllers/admin/admins_controller_test.rb
+++ test/controllers/admin/admins_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class Admin::UsersControllerTest < ActionDispatch::IntegrationTest
+class Admin::AdminsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
