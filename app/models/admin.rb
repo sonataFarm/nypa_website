012ff73b84b84fbcbf58c9e07f9e810790900de3
Nate Festinger <nate.festinger@gmail.com>
2017-11-20 16:42:25 -0800

    Add auth to Admin

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,3 +1,37 @@
 class Admin < ApplicationRecord
-  # TODO
+  validates :username, :password_digest, :session_token, presence: true
+  validates :username, uniqueness: true
+  validates :password, length: { minimum: 6 }
+
+  before_validation :generate_session_token
+
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64
+  end
+
+  def self.find_by_credentials(username, password)
+    admin = Admin.find_by_username(username)
+    admin && admin.is_password?(password) ? admin : nil
+  end
+
+  attr_reader :password
+
+  def password=(password)
+    @password = password
+    @password_digest = BCrypt::Password.create(self.password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def reset_session_token!
+    self.session_token = self.class.generate_session_token
+    self.save
+    self.session_token
+  end
+
+  def ensure_session_token
+    self.session ||= self.class.generate_session_token
+  end
 end
