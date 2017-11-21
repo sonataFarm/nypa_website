8d60af96186c6cbd4bf59d1b05d5cd3f75dae49c
Nate Festinger <nate.festinger@gmail.com>
2017-11-21 13:59:42 -0800

    Modify Node for CoreUI

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,37 @@
-// webpack.config.js
-var path = require('path');
+const webpack = require('webpack');
+const path = require('path');
 
-module.exports = {
-  entry: "./frontend/entry.jsx",
-  output: {
-      path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
-      filename: "bundle.js"
-  },
-  module: {
-    loaders: [
-      {
-        test: [/\.jsx?$/],
-        exclude: /(node_modules)/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['es2015', 'react']
+const BUILD_DIR = path.join(__dirname, 'app', 'assets', 'javascripts');
+const SRC_DIR = path.resolve(__dirname, 'frontend');
+
+module.exports = (env = {}) => {
+  return {
+    entry: {
+      index: [SRC_DIR + '/entry.jsx']
+    },
+    output: {
+      path: BUILD_DIR,
+      filename: '[name].bundle.js'
+    },
+    // watch: true,
+    devtool: 'source-map',
+    resolve: {
+      extensions: ['.js', '.jsx', '*']
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              cacheDirectory: true,
+              presets: ['react', 'env']
+            }
+          }
         }
-      }
-    ]
-  },
-  devtool: 'source-map',
-  resolve: {
-    extensions: ['.js', '.jsx', '*']
+      ]
+    }
   }
 };
