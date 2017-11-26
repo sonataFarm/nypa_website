cf98eee4d682268a4ec81fdc5a3652933c3e2447
Nate Festinger <nate.festinger@gmail.com>
2017-11-26 15:53:02 -0800

    Render CoreUI components

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import {HashRouter, Route, Switch} from 'react-router-dom';
+
+// Containers
+import Full from './containers/Full/'
+
+// Views
+import Login from './views/Pages/Login/'
+import Register from './views/Pages/Register/'
+import Page404 from './views/Pages/Page404/'
+import Page500 from './views/Pages/Page500/'
+
 
 const Root = () => (
-  <div>Hello world!</div>
+  <HashRouter>
+    <Switch>
+      <Route exact path="/login" name="Login Page" component={Login}/>
+      <Route exact path="/register" name="Register Page" component={Register}/>
+      <Route exact path="/404" name="Page 404" component={Page404}/>
+      <Route exact path="/500" name="Page 500" component={Page500}/>
+      <Route path="/" name="Home" component={Full}/>
+    </Switch>
+  </HashRouter>
 );
 
 export default Root;
