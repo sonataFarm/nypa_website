28dd38ed785108d68ee52f775a55477f43abe5a6
Nate Festinger <nate.festinger@gmail.com>
2017-12-30 11:48:12 -0800

    Add validations to Student model

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,11 @@
 class Student < ApplicationRecord
-  validates :name, :current, presence: true
+  validates :first_name, :last_name, presence: true
+  validates :active, inclusion: { in: [ true, false ] }
+  before_validation :ensure_active_status
 
   has_many :awards
+
+  def ensure_active_status
+    self.active ||= true
+  end
 end
