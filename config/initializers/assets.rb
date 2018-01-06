d16efc501862701d96c49cc657feb73a362acb55
Nate Festinger <nate.festinger@gmail.com>
2018-01-05 22:41:59 -0800

    Remove admin assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,7 +12,3 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
-
-Rails.application.config.assets.precompile += %w(admin_bundle.js)
-Rails.application.config.assets.precompile += %w(admin.scss frontend.scss)
-Rails.application.config.assets.precompile += %w(admin.js frontend.js)
